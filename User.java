41cf2ea403de06a6ffe47849d43328f86def117a
itcast <hello@itcast.cn>
2021-02-22 08:42:48 -0500

    fixed problem (can directly commit by add -a)

--- User.java
+++ User.java
@@ -1,8 +1,13 @@
 //这是在b1分支中开发的代码
+//在fix 分支中又做了修改
 public class User{
-	int age;//这是在master分支下的修改
+	int age;//这是在master分支下的修改    
 	String name;//这是在b1分支中的修改
 	public static void main(Strting args[]){
 		System.out.println("Hello World");
+	}
+	//下面内容是在fix分支中做的修改
+	public int getAge{
+		return age;
 	}
 }
